3022bca734883e83d73c7e449b82230a741217b2
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2019-10-06 17:23:26 -0400

    adding examples to vs2019

---
+++ examples/GettingStarted/parallel_for/parallel_average_with_partitioner.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>    //std::max
+#include "tbb/parallel_for.h"
+#include "tbb/blocked_range.h"
+
+using namespace tbb;
+
+struct Average {
+	std::vector<float>& m_vInput;
+	std::vector<float>& m_vOutput;
+	void operator() (const blocked_range<int>& range) const {
+		for (int i = range.begin(); i != range.end(); ++i) {
+			m_vOutput[i] = m_vInput[i];
+			if (i > range.begin())
+				m_vOutput[i] += m_vInput[i - 1]; 
+			if (i < range.end() - 1)
+				m_vOutput[i] += m_vInput[i + 1];
+			m_vOutput[i] /= (1.0f + (float)(i > range.begin()) + (float)(i < range.end() - 1));
+		}
+	}
+	Average(std::vector<float>& vInput, std::vector<float>& vOutput) : m_vInput(vInput), m_vOutput(vOutput) {}
+};
+
+// Note: the input must be padded such that input[n-1] and input[n] can be used
+// to calculate the first and last output values
+void ParallelAverage(std::vector<float>& vInput, std::vector<float>& vOutput, const size_t n) {
+	Average avg(vInput, vOutput);
+	parallel_for(blocked_range<int>(0, n, 1000), avg, auto_partitioner());
+}
+
+int main() {
+	const size_t n = 100;
+	std::vector<float> vInput(n, 0.0f), vOutput(n, 0.0f);
+	for (int i = 0; i < n; ++i)
+		vInput[i] = i * i;
+
+	ParallelAverage(vInput, vOutput, n);
+
+	return 0;
+}
--- examples/GettingStarted/parallel_for/parallel_merge_range.cpp
+++ examples/GettingStarted/parallel_for/parallel_merge_range.cpp
@@ -10,17 +10,55 @@ using namespace tbb;
 template<typename Iterator> 
 
 struct ParallelMergeRange {
-	static size_t grainsize;
-	Iterator begin1, end1; // [begin1,end1) is the first sequence to be merged
-	Iterator begin2, end2; // [begin2, end2) is the 2nd sequence to be merged
-	Iterator out; // where to put merged sequence
-	bool empty() const { return (end1 - begin1) + (end2 - begin2) == 0; }
-	bool is_divisible() const {
-		return std::min(end1 - begin1, end2 - begin2) > grainsize;
-	}
-
+    static size_t grainsize;
+    Iterator begin1, end1; // [begin1,end1) is the first sequence to be merged
+    Iterator begin2, end2; // [begin2, end2) is the 2nd sequence to be merged
+    Iterator out; // where to put merged sequence
+    bool empty() const { return (end1 - begin1) + (end2 - begin2) == 0; }
+    bool is_divisible() const {
+        return std::min(end1 - begin1, end2 - begin2) > grainsize;
+    }
+    ParallelMergeRange(ParallelMergeRange& r, split) {
+        if (r.end1 - r.begin1 < r.end2 - r.begin2) {
+            std::swap(r.begin1, r.begin2);
+            std::swap(r.end1, r.end2);
+        }
+        Iterator m1 = r.begin1 + (r.end1 - r.begin1) / 2;
+        Iterator m2 = std::lower_bound(r.begin2, r.end2, *m1);
+        begin1 = m1;
+        begin2 = m2;
+        end1 = r.end1;
+        end2 = r.end2;
+        out = r.out + (m1 - r.begin1) + (m2 - r.begin2);
+        r.end1 = m1;
+        r.end2 = m2;
+    }
+    ParallelMergeRange(Iterator begin1_, Iterator end1_,
+        Iterator begin2_, Iterator end2_,
+        Iterator out_) :
+        begin1(begin1_), end1(end1_),
+        begin2(begin2_), end2(end2_), out(out_)
+    {}
 };
 
+template<typename Iterator>
+size_t ParallelMergeRange<Iterator>::grainsize = 1000;
+
+template<typename Iterator>
+struct ParallelMergeBody {
+    void operator() (ParallelMergeRange<Iterator>& r) const {
+        std::merge(r.begin1, r.end1, r.begin2, r.end2.r.out);
+    }
+};
+
+template<typename Iterator>
+void ParallelMerge(Iterator begin1, Iterator end1, Iterator begin2, Iterator end2, Iterator out) {
+    parallel_for(
+        ParallelMergeRange<Iterator>(begin1, end1, begin2, end2, out),
+        ParallelMergeBody<Iterator>(),
+        simple_partitioner()
+    )
+}
 
 
 int main() {
